4ba9285d5dd8217322e713ad73716c82b75779e0
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-04 21:10:22 +0200

    add dummy todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from "react";
-import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+	const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<Todos todos={todos} />
+		</>
 	);
 }
 
---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Todo from "../models/todo";
+
+const Todos: React.FC<{ todos: Todo[] }> = (props) => {
+	return (
+		<ul>
+			{props.todos.map((todo) => (
+				<li key={todo.id}>{todo.text}</li>
+			))}
+		</ul>
+	);
+};
+export default Todos;
