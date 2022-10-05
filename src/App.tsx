338a35a86404d5734cc773ea87aab1c847364a25
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-05 02:54:16 +0200

    add todos context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { useState } from "react";
-
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-	const [todos, setTodos] = useState<Todo[]>([]);
-
-	function addTodoHandler(todoText: string) {
-		const newTodo = new Todo(todoText);
-
-		setTodos((prevTodos) => {
-			return prevTodos.concat(newTodo);
-		});
-	}
-
-	function removeTodoHandler(todoId: string) {
-		setTodos((prevTodos) => {
-			return prevTodos.filter((todo) => todo.id !== todoId);
-		});
-	}
-
 	return (
-		<>
-			<NewTodo onAddTodo={addTodoHandler} />
-			<Todos todos={todos} onRemoveTodo={removeTodoHandler} />
-		</>
+		<TodosContextProvider>
+			<NewTodo />
+			<Todos />
+		</TodosContextProvider>
 	);
 }
 
