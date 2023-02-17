e143484db4119a46c5f831da97af047248e181e6
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-17 01:35:46 +0100

    add open and close modal features

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello World");
+const rulesOpenBtn = document.querySelector(".rules-btn") as HTMLButtonElement;
+const rulesCloseBtn = document.querySelector(
+	".rules-btn--close"
+) as HTMLButtonElement;
+
+rulesOpenBtn?.addEventListener("click", () => {
+	document.querySelector(".rules")?.classList.add("active");
+	document.querySelector(".overlay")?.classList.add("active");
+});
+
+rulesCloseBtn?.addEventListener("click", () => {
+	document.querySelector(".rules")?.classList.remove("active");
+	document.querySelector(".overlay")?.classList.remove("active");
+});
