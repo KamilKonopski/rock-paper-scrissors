42f2bab58305ece7292e4b0c3110fc26ce244499
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-18 00:04:59 +0100

    add exclamation mark to variables

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,12 @@
 "use strict";
 const rulesOpenBtn = document.querySelector(".rules-btn");
 const rulesCloseBtn = document.querySelector(".rules-btn--close");
-rulesOpenBtn === null || rulesOpenBtn === void 0 ? void 0 : rulesOpenBtn.addEventListener("click", () => {
+rulesOpenBtn.addEventListener("click", () => {
     var _a, _b;
     (_a = document.querySelector(".rules")) === null || _a === void 0 ? void 0 : _a.classList.add("active");
     (_b = document.querySelector(".overlay")) === null || _b === void 0 ? void 0 : _b.classList.add("active");
 });
-rulesCloseBtn === null || rulesCloseBtn === void 0 ? void 0 : rulesCloseBtn.addEventListener("click", () => {
+rulesCloseBtn.addEventListener("click", () => {
     var _a, _b;
     (_a = document.querySelector(".rules")) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
     (_b = document.querySelector(".overlay")) === null || _b === void 0 ? void 0 : _b.classList.remove("active");
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-const rulesOpenBtn = document.querySelector(".rules-btn") as HTMLButtonElement;
+const rulesOpenBtn = document.querySelector(".rules-btn")! as HTMLButtonElement;
 const rulesCloseBtn = document.querySelector(
 	".rules-btn--close"
-) as HTMLButtonElement;
+)! as HTMLButtonElement;
 
-rulesOpenBtn?.addEventListener("click", () => {
+rulesOpenBtn.addEventListener("click", () => {
 	document.querySelector(".rules")?.classList.add("active");
 	document.querySelector(".overlay")?.classList.add("active");
 });
 
-rulesCloseBtn?.addEventListener("click", () => {
+rulesCloseBtn.addEventListener("click", () => {
 	document.querySelector(".rules")?.classList.remove("active");
 	document.querySelector(".overlay")?.classList.remove("active");
 });
