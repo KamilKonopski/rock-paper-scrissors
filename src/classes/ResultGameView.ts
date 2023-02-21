71e3cb25c527a772fe9b70995230dae3ca71142d
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-21 22:08:56 +0100

    add private static method to create pick element

--- src/classes/ResultGameView.ts
+++ src/classes/ResultGameView.ts
@@ -8,26 +8,6 @@ export default class ResultGameView {
 		const resultGameSection = document.createElement("section");
 		resultGameSection.classList.add("game__result");
 
-		const playerContainer = document.createElement("div");
-		playerContainer.classList.add("player__container");
-
-		const playerHeading = document.createElement("span");
-		playerHeading.classList.add("player__heading");
-		playerHeading.textContent = "you picked";
-		playerContainer.appendChild(playerHeading);
-
-		const playerPick = document.createElement("div");
-		playerPick.classList.add("btn", `btn--${player}`);
-		const playerImgContainer = document.createElement("div");
-		playerImgContainer.classList.add("btn__image-container");
-		const playerImg = document.createElement("img");
-		playerImg.src = `../../images/icon-${player}.svg`;
-		playerImg.alt = player;
-
-		playerImgContainer.appendChild(playerImg);
-		playerPick.appendChild(playerImgContainer);
-		playerContainer.appendChild(playerPick);
-
 		const buttonContainer = document.createElement("div");
 		buttonContainer.classList.add("info__container");
 
@@ -42,30 +22,36 @@ export default class ResultGameView {
 		playAgainButton.addEventListener("click", playAgain);
 		buttonContainer.appendChild(playAgainButton);
 
-		const AIContainer = document.createElement("div");
-		AIContainer.classList.add("ai__container");
-
-		const AIHeading = document.createElement("span");
-		AIHeading.classList.add("ai__heading");
-		AIHeading.textContent = "the house picked";
-		AIContainer.appendChild(AIHeading);
-
-		const AIPick = document.createElement("div");
-		AIPick.classList.add("btn", `btn--${ai}`);
-		const AIImgContainer = document.createElement("div");
-		AIImgContainer.classList.add("btn__image-container");
-		const AIImg = document.createElement("img");
-		AIImg.src = `../../images/icon-${ai}.svg`;
-		AIImg.alt = ai;
-
-		AIImgContainer.appendChild(AIImg);
-		AIPick.appendChild(AIImgContainer);
-		AIContainer.appendChild(AIPick);
-
-		resultGameSection.appendChild(playerContainer);
+		resultGameSection.appendChild(this.createPickElement(player, "you picked"));
 		resultGameSection.appendChild(buttonContainer);
-		resultGameSection.appendChild(AIContainer);
+		resultGameSection.appendChild(
+			this.createPickElement(ai, "the house picked")
+		);
 
 		return resultGameSection;
+	}
+
+	private static createPickElement(pick: string, text: string) {
+		const containerElement = document.createElement("div");
+		containerElement.classList.add(`${pick}__container`);
+
+		const headingElement = document.createElement("span");
+		headingElement.classList.add(`${pick}__heading`);
+		headingElement.textContent = text;
+		containerElement.appendChild(headingElement);
+
+		const pickElement = document.createElement("div");
+		pickElement.classList.add("btn", `btn--${pick}`);
+		const imgContainerElement = document.createElement("div");
+		imgContainerElement.classList.add("btn__image-container");
+		const imgElement = document.createElement("img");
+		imgElement.src = `../../images/icon-${pick}.svg`;
+		imgElement.alt = pick;
+
+		imgContainerElement.appendChild(imgElement);
+		pickElement.appendChild(imgContainerElement);
+		containerElement.appendChild(pickElement);
+
+		return containerElement;
 	}
 }
