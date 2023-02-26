d848938abdd776e277e6c29604e37a7b8c68bec9
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-02-26 20:07:08 +0100

    add dist folder

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+import Game from "./classes/Game.js";
+const game = new Game(0);
+const rulesOpenBtn = document.querySelector(".rules-btn");
+const rulesCloseBtn = document.querySelector(".rules-btn--close");
+rulesOpenBtn.addEventListener("click", () => {
+    var _a, _b;
+    (_a = document.querySelector(".rules")) === null || _a === void 0 ? void 0 : _a.classList.add("active");
+    (_b = document.querySelector(".overlay")) === null || _b === void 0 ? void 0 : _b.classList.add("active");
+});
+rulesCloseBtn.addEventListener("click", () => {
+    var _a, _b;
+    (_a = document.querySelector(".rules")) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
+    (_b = document.querySelector(".overlay")) === null || _b === void 0 ? void 0 : _b.classList.remove("active");
+});
+//# sourceMappingURL=app.js.map
